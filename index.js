ee54f5e775dbc8833e2f68b635cf17099214764c
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-08-31 16:39:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+ 	var f = 0;
+	while(f < facts.length) {
+ 		facts[f] = facts[f] +"!!!";
+  	f++;
+	}
+  return facts;
+}
+
+function iLoveTheBeatles(array) {
+  
+}
